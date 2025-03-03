e7b0bd2c1d9578a74a7d4cc3145a8fdbf086b04d
vandrasiajay <103504448+vandrasiajay@users.noreply.github.com>
2025-03-03 11:06:11 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Cardui from './components/cardUI';
+import Emi from './components/emi';
+import Customerui from './components/Customerui';
+import Product from './components/Product';
+import Custom from './phase2/Custom';
+import Widgets from './phase2/widgets';
+import Rare from './phase2/Rare';
+import Carditem from './phase2/Cartitem';
+import Transactions from './phase2/Transactions';
+const App = () => {
+  return (
+    <div>
+    <Transactions/>
+    </div>
+    
+  );
+};
+
+export default App;
